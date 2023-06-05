760b28c1904292139c549bfc795ee90df7fb2a7b
sushmitha1237 <96813991+sushmitha1237@users.noreply.github.com>
2023-06-06 00:46:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Hello from JS")
+var data1={image : "bz2.jpg",name : "Jeevitha Simma",designation : "Student"}
+var data2={image : "xyz.jpg",name : "Jeevi",designation : "Software Developer"}
+var isdata1 = true;
+var displaydata;
+var changemypicture = function(){
+    if(isdata1){
+        displaydata = data2;
+        isdata1 = false;
+    }
+    else{
+        displaydata = data1;
+        isdata1 = true;
+    }
+    document.getElementById("mypicture").src=displaydata.image;
+    document.getElementById("myname").innerHTML = displaydata.name;
+    document.getElementById("mydesigination").innerHTML = displaydata.designation;
+    
+}
